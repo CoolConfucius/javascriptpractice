caa743a65ded684887b0d53c3be1b71ebe34da3a
Benjamin Huang <benjaminthuang@gmail.com>
2017-02-13 18:20:20 -0800

    slidetoggle is not a function

--- jqueryfunctions.js
+++ jqueryfunctions.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
     console.log("button clicked!");
     $("#addclass > p").addClass("red"); 
   })
+
+  $("#slidetoggle").on('click', 'button', function(event){
+    console.log("button clicked!");
+    // $("#slidetoggle > .imgblock").slideToggle("slow", function(){
+    //   console.log("toggle success!");
+    // }); 
+    $("#imgblock").slideToggle(300); 
+    event.stopPropagation(); 
+  })
 })
