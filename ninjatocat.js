a5c268fcda260a1cceb6476cd63b34c5e1d96ea5
Benjamin Huang <benjaminthuang@gmail.com>
2017-02-15 11:41:21 -0800

    console log attach handlers

--- ninjatocat.js
+++ ninjatocat.js
@@ -1,9 +1,10 @@
 function attach_handlers(){
+  console.log("attach_handlers");
   $(".ninja").on('click', 'img', ninjacat);  
 }
 
 function ninjacat(){
-  console.log("img clicked! callback");
+  console.log("img clicked!");
   console.log($(this).attr("src"));
   console.log($(this).attr("data-alt-src"));
   $(this).attr("src", $(this).attr("data-alt-src"));  
