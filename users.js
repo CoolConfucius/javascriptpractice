6f8b511b236c9f8471740d51876a52b2985a0855
Benjamin Huang <benjaminthuang@gmail.com>
2017-02-15 14:34:06 -0800

    users js clean

--- users.js
+++ users.js
@@ -24,21 +24,4 @@ $(document).ready(function(){
     console.log("Submit!")  
     return false;
   });
-  // $("#addclass").on('click', 'button', function(){
-  //   console.log("button clicked!");
-  //   $("#addclass > p").addClass("red"); 
-  // })
-
-  // $("#slidetoggle").on('click', 'button', function(event){
-  //   console.log("button clicked!");
-  //   $("#imgblock").toggle(300); 
-  //   event.stopPropagation(); 
-  // })
-
-  // $("#append").on('click', 'button', function(){
-  //   console.log("button clicked!");
-  //   var $newp = $("<p>").text("new paragraph!");
-  //   $("#append").append($newp); 
-  // })
-
 })
