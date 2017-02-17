30c9595cfe4ea11df35070b50c3b04d85965f209
Benjamin Huang <benjaminthuang@gmail.com>
2017-02-16 17:48:44 -0800

    pokemon

--- pokemon.js
+++ pokemon.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function(){
 
-  for (var i = 5; i >= 0; i--) {
+  for (var i = 0; i < 5; i++) {
     var head = "http://pokeapi.co/media/img/";
     var tail = ".png";
     var string = head + i.toString() + tail; 
-    var $img = $('<img').attr('src', string); 
+    var $img = $('<img>').attr('src', string); 
     $("body").append($img); 
   };
 })
