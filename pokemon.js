bd7dec3b8d4e18d686f79645d99fd564e60470e3
Benjamin Huang <benjaminthuang@gmail.com>
2017-02-16 18:50:36 -0800

    get

--- pokemon.js
+++ pokemon.js
@@ -1,11 +1,36 @@
 
+function attach_handlers(){
+  console.log("attach_handlers");
+  // $(".pokemon").click(showpokedex);  
+  $(".pokemon").click(showpokedex);  
+}
+
+function showpokedex(e){
+  e.stopPropagation(); 
+  console.log("Pokemon");
+  var id = $(this).attr("id")
+  console.log("Id: ", id);
+  var number = id.substr(1, id.length);
+  console.log(number);
+  $.get("http://pokeapi.co/api/v1/pokemon/"+number+"/", function(res) {
+      console.log(res);
+  }, "json");
+
+  
+}
+
 $(document).ready(function(){
 
-  for (var i = 0; i < 5; i++) {
+  for (var i = 1; i < 5; i++) {
     var head = "http://pokeapi.co/media/img/";
     var tail = ".png";
     var string = head + i.toString() + tail; 
-    var $img = $('<img>').attr('src', string); 
+    var $img = $('<img>').addClass("pokemon").attr('src', string).attr('id', "p"+i); 
     $("body").append($img); 
   };
+  attach_handlers(); 
+
+  // $.get("http://pokeapi.co/api/v1/pokemon/1/", function(res) {
+  //     console.log(res);
+  // }, "json");
 })
