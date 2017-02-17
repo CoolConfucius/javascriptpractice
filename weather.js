a2c324cf45b31996c1396c583418fbb9480921ba
Benjamin Huang <benjaminthuang@gmail.com>
2017-02-16 23:12:00 -0800

    api working

--- weather.js
+++ weather.js
@@ -20,17 +20,19 @@
 // }
 
 $(document).ready(function(){
-  $('form').submit(function() {
-    http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a26a6174d852cdf17527befce1015a88
-
+  $('#form').submit(function() {
       var head = "http://api.openweathermap.org/data/2.5/weather?q=";
       var api = "a26a6174d852cdf17527befce1015a88";
       var tail = "&appid="+api;
       var serialize = $(this).serialize();
+      // console.log(serialize);
+      var city = $("#input").val(); 
+      console.log(city);
+      var url = head + city + tail; 
       // your code here (build up your url)
-      // $.get(url, function(res) {
-          // your code here
-      // }, 'json');
+      $.get(url, function(res) {
+          console.log(res);
+      }, 'json');
       // don't forget to return false so the page doesn't refresh
       return false;
   });
